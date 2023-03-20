44ed1c9b51a7d76d1fd5c92cb0e03b0b69fa6b3e
Martin <68254461+littlee5521@users.noreply.github.com>
2023-03-20 11:15:10 -0700

    lets go, f you undefinded
    caused because i dont know how to use comparators
    apparently

--- mergeSort.js
+++ mergeSort.js
@@ -5,11 +5,12 @@ function mergeSort(x) {
   else {
     const half = Math.ceil(x.length / 2);
     let temp = mergeSort(x.slice(0, half))
-    let temp2 = mergeSort(x.slice(half));
+    console.log("is it here")
     console.log(temp)
+    let temp2 = mergeSort(x.slice(half));
+    console.log("or here")
     console.log(temp2)
     let placeHolder = []
-    console.log("you shoudnt be here")
 
     while (temp.length != 0 || temp2.length != 0) {
 
@@ -17,20 +18,22 @@ function mergeSort(x) {
       console.log(temp2[0])
       if (temp[0] < temp2[0]) {
         placeHolder.push(temp.shift())
-        if (temp == 0) {
+        if (temp == 0 && temp2.length != 0) {
           placeHolder.push(temp2.shift())
         }
       }
       else {
-        placeHolder.push(temp2.shift())
-        if (temp2 == 0) {
+        if (temp2[0] != undefined) {
+          placeHolder.push(temp2.shift())
+        }
+
+        if (temp2 == 0 && temp.length != 0) {
           placeHolder.push(temp.shift())
         }
       }
     }
-    console.log(placeHolder)
     return placeHolder
   }
 }
 
-console.log(mergeSort([5, 2, 1, 9, 7]))
+console.log(mergeSort([4, 2, 1, 9, 7]))
